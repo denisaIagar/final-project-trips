4954af7ca069869fa751e67852e84509b4a20ac5
denisaIagar <iagar.denisa@gmail.com>
2019-04-24 19:37:11 +0300

    first commit

---
+++ src/main/java/ro/siit/denisa/MyTripsApp/MyTripsAppApplication.java
@@ -0,0 +1,13 @@
+package ro.siit.denisa.MyTripsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTripsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTripsAppApplication.class, args);
+	}
+
+}
